6f7f3998fa1f13a78da04bbc4e087a19a81030bd
REN-YA <re_n_ya@icloud.com>
2022-02-25 13:22:04 +0000

    Update header・layout

--- app/controllers/store/parameter_sanitizer.rb
+++ app/controllers/store/parameter_sanitizer.rb
@@ -2,7 +2,7 @@ class Store::ParameterSanitizer < Devise::ParameterSanitizer
 
   def initialize(*)
     super
-    permit(:sign_up, keys: [:name, :email, :encrypted_password, :image_id, :store_genres, :post_code, :address, :business_hours, :reservation_site, :telephone_number] )
+    permit(:sign_up, keys: [:name, :email, :encrypted_password, :image, :store_genres, :post_code, :address, :business_hours, :reservation_site, :telephone_number] )
   end
 
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -44,7 +44,7 @@ class User::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :encrypted_password, :image_id)
+    params.require(:user).permit(:name, :email, :encrypted_password, :image)
   end
 
 end
